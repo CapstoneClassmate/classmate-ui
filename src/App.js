fbd1703d72cdd8d54ea8e2b4ddedbdbe20c3ac25
Sean Waters <skwaters@uark.edu>
2019-01-23 20:00:37 -0600

    login shit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+/*import ReactDOM from 'react-dom';*/
+import GoogleLogin from 'react-google-login';
+import config from './config.json'
+/*import logo from './logo.svg';*/
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { isAuthenticated: false, user: null, token: ''};
+  }
+
+  logout = () => {
+    this.setState({isAuthenticated: false, token: '', user: null})
+  };
+
+  onFailure = (error) => {
+    alert(error);
+  };
+
+  googleResponse = (response) => {
+    console.log(response);
+  }
+
   render() {
+    let content = !!this.state.isAuthenticated ?
+        (
+          <div>
+            <p>Authenticated</p>
+            <div>
+              {this.state.user.email}
+            </div>
+            <div>
+              <button onClick={this.logout} className="button">
+                  Log out
+              </button>
+            </div>
+          </div>
+        ) :
+        (
+          <div>
+            <GoogleLogin
+              clientId={config.GOOGLE_CLIENT_ID}
+              buttonText="Login"
+              onSuccess={this.googleResponse}
+              onFailure={this.onFailure}
+            />
+          </div>
+        );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {content}
       </div>
     );
   }
