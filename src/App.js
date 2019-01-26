41f7b866076c47f4143e138cdf56455f5aab18f7
malec <alec@ahlbrandt.net>
2019-01-26 17:18:12 -0600

    Add endpoint url and formatting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import config from './config.json'
-//import './App.css';
+import * as util from './util';
 
 class App extends Component {
 
   constructor() {
     super();
-    this.state = { isAuthenticated: false, user: null, token: ''};
+    this.state = { isAuthenticated: false, user: null, token: '' };
   }
 
   logout = () => {
-    this.setState({isAuthenticated: false, token: '', user: null})
+    this.setState({ isAuthenticated: false, token: '', user: null })
   };
 
   onFailure = (error) => {
@@ -19,18 +19,18 @@ class App extends Component {
   };
 
   googleResponse = (response) => {
-    const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+    const tokenBlob = new Blob([JSON.stringify({ access_token: response.accessToken }, null, 2)], { type: 'application/json' });
     const options = {
       method: 'POST',
       body: tokenBlob,
       mode: 'cors',
       cache: 'default'
     };
-    fetch('http://localhost:4000/api/v1/auth/google', options).then(r => {
+    fetch(util.ENDPOINT_URL + '/api/v1/auth/google', options).then(r => {
       const token = r.headers.get('x-auth-token');
       r.json().then(user => {
         if (token) {
-          this.setState({isAuthenticated: true, user, token})
+          this.setState({ isAuthenticated: true, user, token })
         }
       });
     })
@@ -38,29 +38,29 @@ class App extends Component {
 
   render() {
     let content = !!this.state.isAuthenticated ?
-    (
-      <div>
-        <p>Authenticated</p>
+      (
         <div>
-          {this.state.user.email}
-        </div>
-        <div>
-          <button onClick={this.logout} className="button">
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button">
               Log out
           </button>
+          </div>
         </div>
-      </div>
-    ) :
-    (
-      <div>
-        <GoogleLogin
-          clientId={config.GOOGLE_CLIENT_ID}
-          buttonText="Login"
-          onSuccess={this.googleResponse}
-          onFailure={this.onFailure}
-        />
-      </div>
-    );
+      ) :
+      (
+        <div>
+          <GoogleLogin
+            clientId={config.GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={this.googleResponse}
+            onFailure={this.onFailure}
+          />
+        </div>
+      );
     return (
       <div className="App">
         {content}
