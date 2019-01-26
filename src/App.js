28a5cdbdc470040fc9c9cf808f1135cd1ce0ad23
Sean Waters <skwaters@uark.edu>
2019-01-26 12:16:08 -0600

    Login feature working locally

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-/*import ReactDOM from 'react-dom';*/
-import GoogleLogin from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 import config from './config.json'
-/*import logo from './logo.svg';*/
-import './App.css';
+//import './App.css';
 
 class App extends Component {
 
@@ -21,36 +19,50 @@ class App extends Component {
   };
 
   googleResponse = (response) => {
-    console.log(response);
-  }
+    const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+    const options = {
+      method: 'POST',
+      body: tokenBlob,
+      mode: 'cors',
+      cache: 'default'
+    };
+    fetch('http://localhost:4000/api/v1/auth/google', options).then(r => {
+      const token = r.headers.get('x-auth-token');
+      r.json().then(user => {
+        if (token) {
+          this.setState({isAuthenticated: true, user, token})
+        }
+      });
+    })
+  };
 
   render() {
     let content = !!this.state.isAuthenticated ?
-        (
-          <div>
-            <p>Authenticated</p>
-            <div>
-              {this.state.user.email}
-            </div>
-            <div>
-              <button onClick={this.logout} className="button">
-                  Log out
-              </button>
-            </div>
-          </div>
-        ) :
-        (
-          <div>
-            <GoogleLogin
-              clientId={config.GOOGLE_CLIENT_ID}
-              buttonText="Login"
-              onSuccess={this.googleResponse}
-              onFailure={this.onFailure}
-            />
-          </div>
-        );
-    return (
+    (
       <div>
+        <p>Authenticated</p>
+        <div>
+          {this.state.user.email}
+        </div>
+        <div>
+          <button onClick={this.logout} className="button">
+              Log out
+          </button>
+        </div>
+      </div>
+    ) :
+    (
+      <div>
+        <GoogleLogin
+          clientId={config.GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={this.googleResponse}
+          onFailure={this.onFailure}
+        />
+      </div>
+    );
+    return (
+      <div className="App">
         {content}
       </div>
     );
