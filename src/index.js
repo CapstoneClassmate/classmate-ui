a4b9b6c67427d64dfd3ac95e4900ea06aa749939
Jack Sullivan <sullivanjack87@gmail.com>
2019-02-05 15:17:15 -0600

    Added some shit
    Alec fix it you god.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 
+
+
+function NavbarC(props) {
+    return  <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="index.js">Classmate</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="login.js">Login</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+            </Navbar>;
+}
+
+const element = <NavbarC />;
+ReactDOM.render(element, document.getElementById('navbar'));
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
---
+++ src/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import NavbarC from "index.js"
+
+const element = <NavbarC />;
+//ReactDOM.render(element, document.getElementById('navbar'));
+
+const e = <h1>Jack></h1>;
+ReactDOM.render(e, document.getElementById('jack');
