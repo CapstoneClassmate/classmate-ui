422ed23e21054652a043557fbd660371fa17f37e
malec <alec@ahlbrandt.net>
2019-02-05 16:20:22 -0600

    update styling

--- src/util.js
+++ src/util.js
@@ -1,3 +1,3 @@
-var getENDPOINT_URL = () => { process.env.REACT_APP_ENDPOINT_URL || "http://localhost:4000" }
+var getENDPOINT_URL = () => reutrn (process.env.REACT_APP_ENDPOINT_URL || "http://localhost:4000");
 
 module.exports = { getENDPOINT_URL };
