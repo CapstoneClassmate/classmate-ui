692f82dc5a22abf1de6e0801c43581e6bbd17efc
malec <alec@ahlbrandt.net>
2019-02-05 16:11:46 -0600

    use react_app_url

--- src/util.js
+++ src/util.js
@@ -1,10 +1,3 @@
-var getENDPOINT_URL = () => { 
-    console.log('node env', process.env.NODE_ENV);
-    if (process.env.NODE_ENV === "production") {
-        return "http://classmate-endpoint-prod.herokuapp.com";
-    } else {
-        return "http://localhost:4000";
-    }
-};
+var getENDPOINT_URL = () => { process.env.REACT_APP_ENDPOINT_URL || "http://localhost:4000" }
 
 module.exports = { getENDPOINT_URL };
