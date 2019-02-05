ddc1badd24657791bd9eb17ee9cf08c66c57e70f
malec <alec@ahlbrandt.net>
2019-02-05 16:05:33 -0600

    log node_env

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 var getENDPOINT_URL = () => { 
+    console.log('node env', process.env.NODE_ENV);
     if (process.env.NODE_ENV === "production") {
         return "http://classmate-endpoint-prod.herokuapp.com";
     } else {
